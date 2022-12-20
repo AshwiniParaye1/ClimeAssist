24333b9a86198b8c64272f609223231b729c4567
Ashwini <ashwiniparaye9309@gmail.com>
2022-12-20 19:06:57 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import Inputs from './components/Inputs';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
 import TimeAndLocation from './components/TimeAndLocation';
 import TopButtons from './components/TopButtons';
-import getWeatherData from './services/WeatherService';
+import getFormattedWeatherData from './services/WeatherService';
 // import UilReact from '@iconscout/react-unicons/icons/uil-react';    
 
 function App() {
 
   const fetchWeather = async () => {
 
-    const data = await getWeatherData('weather', {q: 'aurangabad'});
+    const data = await getFormattedWeatherData({q: 'aurangabad'});
     console.log(data);
 
   }
 
-fetchWeather();
+  fetchWeather();
 
   return (
       <div className='mx-auto max-w-screen-md mt-4 py-5 px-14 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -10,9 +10,42 @@ const getWeatherData = ( infoType, searchParams ) => {
     const url = new URL(BASE_URL + '/' + infoType)
     url.search = new URLSearchParams({...searchParams, appid:API_KEY})
 
+    console.log(url);
+
     return fetch(url)
     .then((res) => res.json())
-    .then((data) => data);
+    // .then((data) => data);
 };
 
-export default getWeatherData;
+
+const formatCurrentWeather = (data) => {
+
+    const {
+
+        coord: {lat, lon},
+        main: {temp, feels_like, temp_min, temp_max, humidity},
+        name,
+        dt,
+        sys: {country, sunrise, sunset},
+        weather,
+        wind: {speed},
+
+    } = data
+
+    const {main: details, icon} = weather[0];
+
+    return {
+        lat, lon, temp, feels_like, temp_min, temp_max, humidity, name, dt, country, sunrise, sunset, details, icon, speed,
+    };
+
+}
+
+const getFormattedWeatherData = async (searchParams) => {
+
+    const formattedCurrentWeather = await getWeatherData('weather', searchParams).then(formatCurrentWeather)
+
+    return formattedCurrentWeather
+}
+
+
+export default getFormattedWeatherData;
